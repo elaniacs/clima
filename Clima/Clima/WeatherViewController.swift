4c0aebebf601f326b33df4a4d70f66e193153a37
elaniacs <carensousaesilva@gmail.com>
2022-09-08 11:13:04 -0300

    add Storyboard, Assets and IBOutlets

---
+++ Clima/Clima/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Cáren Sousa on 08/09/22.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
